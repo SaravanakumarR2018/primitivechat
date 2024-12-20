8375ca5bfa93f9dc4f76c24b4dcc2afda88375c3
santhiya-mj <sathiyamurugesan2024@outlook.com>
2024-12-20 12:49:31 +0530

    Updated run_server.sh file

--- build/run_server.sh
+++ build/run_server.sh
@@ -15,6 +15,13 @@ echo "PROJECT_ROOT is set to: $PROJECT_ROOT"
 if [ -f "$PROJECT_ROOT/src/backend/.env" ]; then
     echo "Loading environment variables from .env file..."
     export $(grep -v '^#' "$PROJECT_ROOT/src/backend/.env" | xargs)
+
+    echo "Exported environment variables:"
+    while IFS='=' read -r key value; do
+        [[ "$key" =~ ^#.*$ || -z "$key" ]] && continue  # Skip comments and empty lines
+        echo "$key=${!key}"  # Print the variable name and its value
+    done < <(grep -v '^#' "$PROJECT_ROOT/src/backend/.env" | xargs -n1 echo)
+
 else
     echo ".env file not found in $PROJECT_ROOT/src/backend/. Exiting..."
     exit 1
