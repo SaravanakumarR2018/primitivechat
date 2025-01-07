156acd17484688de041c0e0bf4dbc4c3bc18303e
Bharanitharan <109167711+Bharani0012@users.noreply.github.com>
2025-01-07 09:00:03 +0530

    Update test_get_ticket_by_id.py

--- test/IntegrationTests/TicketService/test_get_ticket_by_id.py
+++ test/IntegrationTests/TicketService/test_get_ticket_by_id.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 class TestGetTicketEndpoint(unittest.TestCase):
-    BASE_URL = f"http://localhost:{os.getenv('CHAT_SERVICE_PORT', 8000)}"
+    BASE_URL = f"http://localhost:{os.getenv('CHAT_SERVICE_PORT')}"
     allowed_custom_field_sql_types = ["VARCHAR(255)", "INT", "BOOLEAN", "DATETIME", "MEDIUMTEXT", "FLOAT", "TEXT"]
 
     def setUp(self):
