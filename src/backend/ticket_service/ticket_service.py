32aa3c2446872f24b89a581b4a287a1f6dd28334
Bharani0012 <bharanitharan695@gmail.com>
2025-02-24 09:24:33 +0530

    updated ticket_service.py

--- src/backend/ticket_service/ticket_service.py
+++ src/backend/ticket_service/ticket_service.py
@@ -87,8 +87,9 @@ class TicketByCustomerId(BaseModel):
     ticket_id: str
     title: str
     status: str
-    description: str
     priority:str
+    reported_by:str
+    assigned:str
     created_at:datetime
 
 #Comments Pydantic Model
