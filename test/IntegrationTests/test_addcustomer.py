48efaeea52485898d07085cf97e461364e811a5e
Kabilan A <147593493+Kabilan-16@users.noreply.github.com>
2025-03-14 18:53:29 +0530

    Update test_addcustomer.py

--- test/IntegrationTests/test_addcustomer.py
+++ test/IntegrationTests/test_addcustomer.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import unittest
-
-from utils.api_utils import add_customer
+import requests
+from utils.api_utils import add_customer, create_token_without_org_id, create_token_without_org_role
 
 # Set up logging configuration
 logging.basicConfig(level=logging.INFO)
@@ -29,6 +29,60 @@ class TestAddCustomerAPI(unittest.TestCase):
         self.assertTrue(data["org_id"].startswith(start_org_id), f"org_id does not start with {start_org_id}")
 
         logger.info("Test completed successfully for test_add_customer.")
+        
+    def test_addcustomer_without_token(self):
+        logger.info("Executing addcustomer_without_token: Testing error handling for missing token")
+
+        url = f"{self.BASE_URL}/addcustomer"
+        response = requests.post(url)
+        logger.info(f"OUTPUT: Response status code: {response.status_code}")
+        logger.info(f"OUTPUT: Response content: {response.text}")
+
+        self.assertEqual(response.status_code, 401)
+        response_json = response.json()
+        self.assertEqual(response_json.get("detail"), "Authentication required")
+        logger.info("Test completed successfully for test_addcustomer_without_token.")
+
+    def test_corrupted_token(self):
+        """Test API request with a corrupted authentication token."""
+        headers = {"Authorization": "Bearer corrupted_token"}
+        url = f"{self.BASE_URL}/addcustomer"
+
+        logger.info("Testing API request with corrupted token")
+        response = requests.post(url, headers=headers)
+        self.assertEqual(response.status_code, 401, "Corrupted token should result in 401 Unauthorized")
+        self.assertEqual(response.json()["detail"], "Authentication required", "Unexpected error message")
+        logger.info("Test completed successfully test_corrupted_token")
+
+    def test_addcustomer_token_without_org_role(self):
+        logger.info("Testing addcustomer API with a token missing org_role")
+        customer_data = add_customer("test_org")
+        org_id = customer_data.get("org_id")
+
+        headers = {'Authorization': f'Bearer {create_token_without_org_role(org_id)}'}
+        url = f"{self.BASE_URL}/addcustomer"
+        response = requests.post(url, headers=headers)
+
+        self.assertEqual(response.status_code, 403, "Expected status code 403 for missing org_role")
+        self.assertIn("detail", response.json(), "'detail' key not found in response")
+        self.assertEqual(response.json()["detail"], "Forbidden: Insufficient role", "Unexpected error message")
+
+        logger.info("Test completed successfully test_addcustomer_token_without_org_role")
+
+    def test_addcustomer_token_without_org_id(self):
+        logger.info("Testing addcustomerAPI with a token missing org_id")
+
+        headers = {'Authorization': f'Bearer {create_token_without_org_id("org:admin")}'}
+        url = f"{self.BASE_URL}/addcustomer"
+        response = requests.post(url, headers=headers)
+
+        # Expect 400 Bad Request
+        self.assertEqual(response.status_code, 400, "Expected status code 400 for missing org_id")
+        self.assertIn("detail", response.json(), "'detail' key not found in response")
+        self.assertEqual(response.json()["detail"], "Org ID not found in token", "Unexpected error message")
+
+        logger.info("Test completed successfully test_addcustomer_token_without_org_id")
+
 
 if __name__ == "__main__":
     unittest.main()
