2270947093600eda3b9f4528da1769c3e40c0b7c
Kabilan A <147593493+Kabilan-16@users.noreply.github.com>
2025-03-21 12:10:54 +0530

    Update test_fileembeddingstatus.py

--- test/IntegrationTests/FileVectorizer/test_fileembeddingstatus.py
+++ test/IntegrationTests/FileVectorizer/test_fileembeddingstatus.py
@@ -6,6 +6,7 @@ import requests
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 from utils.api_utils import add_customer, create_test_token, create_token_without_org_role, create_token_without_org_id
+from src.backend.lib.logging_config import log_format
 
 # Set up logging configuration
 logging.basicConfig(level=logging.INFO, format=log_format)
