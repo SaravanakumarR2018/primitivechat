7ae561bc2076083a29fd88090c889425bb9dc219
Bharanitharan <109167711+Bharani0012@users.noreply.github.com>
2025-04-02 15:03:07 +0530

    Update ticket_service.py

--- src/backend/ticket_service/ticket_service.py
+++ src/backend/ticket_service/ticket_service.py
@@ -13,7 +13,6 @@ from src.backend.lib.utils import CustomerService, auth_admin_dependency
 from src.backend.lib.logging_config import log_format
 
 # Setup logging configuration
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s:%(lineno)d %(funcName)s %(levelname)s %(message)s')
 logging.basicConfig(level=logging.DEBUG, format=log_format)
 logger = logging.getLogger(__name__)
 
