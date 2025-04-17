9c0f930bffdfb770a197fe8c5ee2b5da1cc47f35
Saravana Kumar Rajendran <srajendran@microsoft.com>
2025-04-17 09:35:52 +0530

    Fix generate Token file (#183)

--- build/generate_token/generate_token.sh
+++ build/generate_token/generate_token.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Get the Git root directory
-GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
-
-# Change to the Git root directory
-cd "$GIT_ROOT_DIR"
-
-# Get the directory of the current script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Change to the script directory
-cd "$SCRIPT_DIR"
-
 # Default values
-ORG_ID="org_2wxyzABCdEfGHIjkLmnotw" 
+ORG_ID="org_2wxyzABCdEfGHIjkLmnotw"
 ORG_ROLE="org:admin"
 
 # Function to display help
@@ -26,6 +14,7 @@ function display_help() {
     echo "  -h, --help        Display this help message"
 }
 
+# Parse CLI arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -i|--org-id)
@@ -46,25 +35,45 @@ while [[ "$#" -gt 0 ]]; do
             exit 1
             ;;
     esac
-done  # Removed the 'shift' here
+done
 
-# Check if ORG_ID is provided
+# Validate ORG_ID
 if [ -z "$ORG_ID" ]; then
     echo "Error: Organization ID is required."
     display_help
     exit 1
 fi
 
-# Check if ORG_ROLE is valid
+# Validate ORG_ROLE
 if [[ ! " org:admin org:member org:user org:super_admin " =~ " $ORG_ROLE " ]]; then
     echo "Error: Invalid organization role."
     display_help
     exit 1
 fi
 
-# Always build the Docker image
-echo "Building the Docker image..."
-docker build -t generate-token-image -f build/generate_token/Dockerfile .
+# Resolve script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Run the Docker container to generate the token
-docker run --rm -e ORG_ID="$ORG_ID" -e ORG_ROLE="$ORG_ROLE" -v "$GIT_ROOT_DIR":/app -w /app generate-token-image python -c "import sys; from test.IntegrationTests.utils.api_utils import create_test_token; print(create_test_token(sys.argv[1], sys.argv[2]))" "$ORG_ID" "$ORG_ROLE" 
+# Try to get Git root directory
+GIT_ROOT_DIR=$(git -C "$SCRIPT_DIR" rev-parse --show-toplevel 2>/dev/null)
+if [ -z "$GIT_ROOT_DIR" ]; then
+    echo "Error: Could not determine Git root. Make sure this script is inside a Git repo."
+    exit 1
+fi
+
+echo "Building the Docker image..."
+docker build -t generate-token-image -f "$SCRIPT_DIR/Dockerfile" "$SCRIPT_DIR"
+if [ $? -ne 0 ]; then
+    echo "Docker build failed."
+    exit 1
+fi
+
+echo "Running Docker container to generate token..."
+docker run --rm \
+    -e ORG_ID="$ORG_ID" \
+    -e ORG_ROLE="$ORG_ROLE" \
+    -v "$GIT_ROOT_DIR":/app \
+    -w /app \
+    generate-token-image \
+    python -c "import sys; from test.IntegrationTests.utils.api_utils import create_test_token; print(create_test_token(sys.argv[1], sys.argv[2]))" "$ORG_ID" "$ORG_ROLE"
+
