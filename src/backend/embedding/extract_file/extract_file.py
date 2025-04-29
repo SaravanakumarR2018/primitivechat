6d7758ba732197ccdbb394d27478b24ee7eeaf80
Kabilan A <147593493+Kabilan-16@users.noreply.github.com>
2025-04-29 16:44:34 +0530

    Update extract_file.py

--- src/backend/embedding/extract_file/extract_file.py
+++ src/backend/embedding/extract_file/extract_file.py
@@ -26,7 +26,6 @@ import re
 import yaml
 from src.backend.embedding.lib.download_and_upload_file import LocalFileDownloadAndUpload
 
-from src.backend.lib.logging_config import log_format
 from src.backend.lib.singleton_class import Singleton
 
 from src.backend.lib.logging_config import get_primitivechat_logger
